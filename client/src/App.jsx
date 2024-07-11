6af5d7fff3648bb2bbb337859068ce1ed9181834
Joy Maggie <maggieawino17@gmail.com>
2024-07-11 16:50:06 +0300

    setup pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Search from './pages/Search'
+import Signup from './pages/Signup'
+import Collections from './pages/Collections'
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/search' element={<Search/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/collections' element={<Collections/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
