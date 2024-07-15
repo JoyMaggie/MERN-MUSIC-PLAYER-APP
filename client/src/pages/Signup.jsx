8b44011f70e0a2e51c09f7acc74bda25e5ea036c
Joy Maggie <maggieawino17@gmail.com>
2024-07-15 18:32:39 +0300

    Add functionality to the signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,11 +1,51 @@
-import { Button, Label, TextInput } from 'flowbite-react';
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import React, { useState } from 'react'
 import { FaSpotify } from "react-icons/fa6";
 import { FcGoogle } from "react-icons/fc";
-import { FaFacebook, FaApple } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { HiInformationCircle } from "react-icons/hi";
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Signup() {
+  const[formData, setFormData] = useState({})
+  const[errorMessage, setErrorMessage] = useState(null)
+  const[loading,setLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const handleChange = (e)=>{
+    const{value, id} = e.target
+    setFormData({...formData, [id]:value})
+  }
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault()
+
+    if(!formData.username || !formData.email|| !formData.password){
+      return setErrorMessage("Please fill out all fields!!")
+    }
+
+    try {
+      setLoading(true)
+      setErrorMessage(null)
+      const res = await fetch('/api/auth/signup',{
+        method:'POST',
+        headers:{'Content-Type':'application/json'},
+        body:JSON.stringify(formData)
+      })
+      const data = await res.json()
+
+      if(data.success === false){
+        return setErrorMessage(data.message)
+      }
+      setLoading(false)
+      if(res.ok){
+        navigate('/')
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setLoading(false)
+    }
+  }
+  
   return (
     <div className='min-h-screen mt-12'>
       <div className='mx-auto max-w-sm flex flex-col p-5'>
@@ -14,18 +54,40 @@ export default function Signup() {
           <h1 className='text-2xl font-serif font-semibold'>Sign up to start listening</h1>
         </div>
 
-        <form className='flex flex-col gap-2 mt-7'>
-          <Label value='Email Address
-          '/>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-3 mt-7'>
+          <Label value='Username'/>
+          <TextInput
+            id='username'
+            type='text'
+            placeholder='username'
+            onChange={handleChange}/>
+
+          <Label value='Email Address'/>
           <TextInput
             id='email'
             type='email'
             placeholder='name@smolify.com'
-            required/>
-            <Button type='submit' gradientDuoTone="redToYellow"  pill>Next </Button>
+            onChange={handleChange}/>
+
+          <Label value='Password'/>
+          <TextInput
+            id='password'
+            type='password'
+            placeholder='xxxxxxxxxxxxx'
+            onChange={handleChange}/>
+            <Button 
+              type='submit' gradientDuoTone="redToYellow"  
+              pill
+              disabled={loading}>
+                {loading ? 
+                  (<>
+                    <Spinner size='sm'/>
+                    <span>Loading...</span>
+                    </>) : "Sign Up"}
+            </Button>
         </form>
 
-        <div className='flex items-center justify-center my-3'>
+        <div className='flex items-center justify-center my-1'>
           <span>or</span>
         </div>
 
@@ -36,26 +98,16 @@ export default function Signup() {
               <FcGoogle className='w-6 h-6 mr-6 '/>
               Sign up with Google
             </Button>
-          
-            <Button 
-              className='text-lg' 
-              pill 
-              outline gradientDuoTone="redToYellow">
-              <FaFacebook className='w-6 h-6 mr-6'/>Sign up with Facebook
-            </Button>
-
-            <Button 
-              className='text-lg' 
-              pill 
-              outline gradientDuoTone="redToYellow">
-              <FaApple className='w-6 h-6 mr-6'/>Sign up with Apple
-            </Button>
         </div>
 
         <div className='mt-2 text-center'>
           <span>Already have an account?</span>
           <Link to="/login" className='underline'>Log in here</Link>
         </div>
+
+        {errorMessage &&
+          <Alert className='mt-4' color='failure' icon={HiInformationCircle}>{errorMessage}</Alert>
+        }
       </div>
     </div>
   )
